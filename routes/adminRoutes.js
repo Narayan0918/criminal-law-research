3c03781a5a03c349be48b769e2a18abcc74f7d5e
Narayan <narayanrajdubey9889@gmail.com>
2025-09-27 18:22:22 +0530

    changes made in adminRoutes as there were some duplicate definition

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,27 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { registerAdmin, loginAdmin } = require('../controllers/adminController');
-
-router.post('/register', registerAdmin);
-router.post('/login', loginAdmin);
-
-module.exports = router;const express = require('express');
-const { 
-  registerAdmin, 
+module.exports = router;
+const {
+  registerAdmin,
   loginAdmin,
   getAllAdmins,
   approveAdmin,
   deleteAdmin,
-} = require('../controllers/adminController');
-const { protect, isSuperAdmin } = require('../middleware/authMiddleware');
+} = require("../controllers/adminController");
+const { protect, isSuperAdmin } = require("../middleware/authMiddleware");
 
 // Public routes
-router.post('/register', registerAdmin);
-router.post('/login', loginAdmin);
+router.post("/register", registerAdmin);
+router.post("/login", loginAdmin);
 
 // Super-admin only routes
-router.route('/').get(protect, isSuperAdmin, getAllAdmins);
-router.route('/:id').delete(protect, isSuperAdmin, deleteAdmin);
-router.route('/:id/approve').put(protect, isSuperAdmin, approveAdmin);
+router.route("/").get(protect, isSuperAdmin, getAllAdmins);
+router.route("/:id").delete(protect, isSuperAdmin, deleteAdmin);
+router.route("/:id/approve").put(protect, isSuperAdmin, approveAdmin);
 
 module.exports = router;
