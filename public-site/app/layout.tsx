3551f1db8f26ed03ac463540c27d5ad5ca942c5d
Narayan <narayanrajdubey9889@gmail.com>
2025-10-03 17:50:28 +0530

    updated admin url from local to server

--- public-site/app/layout.tsx
+++ public-site/app/layout.tsx
@@ -61,7 +61,7 @@ export default function RootLayout({
           {/* --- START: Added Admin Login Link --- */}
           <div className="mt-2">
             <a
-              href="http://localhost:3001"
+              href="https://criminal-law-research-apkanm23n-narayan0918s-projects.vercel.app"
               target="_blank"
               rel="noopener noreferrer"
               className="text-sm text-gray-800 hover:text-accent"
